0d82def1450611f30426e3a3b7d8a93a3e7451a2
Pablo Israel <pabloisrael@Pablos-MacBook-Pro.local>
2020-10-21 18:37:56 -0300

    Add **/.DS_Store to gitignore

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,4 @@
 
-
-
-// app.post('/auth', (req, res) => {
-//     console.log(req);
-//     logIn(3);
-//     res.send('Puto el que lee!')
-// });
-
 exports.logIn = (req, res) => {
     console.log(req.body);
     res.send("Nos re vimos")
